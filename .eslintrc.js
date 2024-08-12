b33d7a1e6b2c4d6158bff015ec993610196e3e59
evrwaq <evrwaq@gmail.com>
2024-08-12 15:29:00 -0300

    refactor: change eslint config and update package.json

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+  },
+  "extends": [
+    "eslint:recommended",
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module",
+  },
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "single"],
+    "indent": ["error", 2],
+    "no-unused-vars": ["warn"],
+  },
+};
