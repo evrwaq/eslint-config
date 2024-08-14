b5f60980fe54aa7176302b22c8e17f101f1371a4
evrwaq <evrwaq@gmail.com>
2024-08-14 19:25:52 -0300

    chore: change eslint config to modules and add typescript-eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
+  }
+];
