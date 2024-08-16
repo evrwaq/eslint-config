c99f49cc3f1a3c3b6eab883a3b6faf56bfc8adc8
evrwaq <evrwaq@gmail.com>
2024-08-15 22:41:16 -0300

    chore: add eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,18 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
     rules: {
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2],
       '@typescript-eslint/no-unused-vars': ['warn'],
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn'
-    }
-  }
-];
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/adjacent-overload-signatures': 'error',
+      '@typescript-eslint/array-type': 'error',
+    },
+  },
+]
