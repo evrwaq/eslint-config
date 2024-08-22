96ba8f61edf3f99183faea6eb83ccfbbc2967749
evrwaq <evrwaq@gmail.com>
2024-08-21 22:54:49 -0300

    chore: add rule to enums

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -47,7 +47,8 @@ export default [
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/adjacent-overload-signatures': 'error',
       '@typescript-eslint/array-type': 'error',
-      '@typescript-eslint/consistent-type-exports': 'error'
+      '@typescript-eslint/consistent-type-exports': 'error',
+      '@typescript-eslint/no-mixed-enums': 'error'
     },
   },
 ]
