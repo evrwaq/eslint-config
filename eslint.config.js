25e4fadcf6fbc59c91ed9a22bd717f75b4beb52b
evrwaq <evrwaq@gmail.com>
2024-08-22 23:17:27 -0300

    chore: remove typescript-eslint and eslint to @stylistic

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import stylisticTs from '@stylistic/eslint-plugin-ts'
+import parserTs from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+    },
+    plugins: {
+      '@stylistic/ts': stylisticTs,
+    },
+    rules: {
+      'eol-last': ['error', 'always'],
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/ts/quotes': ['error', 'single'],
+      '@stylistic/ts/semi': ['error', 'never'],
+    },
+  },
+]
