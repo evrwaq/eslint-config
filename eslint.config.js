790ea52112cb22aad11bd0d6e48baf0a453f621c
evrwaq <evrwaq@gmail.com>
2024-08-23 23:39:38 -0300

    chore: add rules to class and methods spacing

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,18 @@ export default [
     },
     rules: {
       'eol-last': ['error', 'always'],
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'class' },
+        { blankLine: 'always', prev: 'class', next: '*' },
+        { blankLine: 'always', prev: 'block-like', next: 'block-like' },
+        { blankLine: 'any', prev: 'block-like', next: 'block-like' },
+      ],
+      'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: false }],
+      'no-multiple-empty-lines': [
+        'error', 
+        { max: 1, maxEOF: 0, maxBOF: 0 }
+      ],
       '@stylistic/indent': ['error', 2],
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/semi': ['error', 'never'],
